448f5d522662f38f2caed2450b706e7682a510a8
David Konsumer <konsumer@jetboystudio.com>
2024-03-15 02:28:21 -0700

    cleanup & simplify

--- firmware/firmware.py
+++ firmware/firmware.py
@@ -1,76 +1,18 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 from PIL import Image, ImageSequence
-from luma.core.sprite_system import framerate_regulator
-from luma.core import cmdline, error
-import sys
+from oled import runloop, framerate_regulator
+from pathlib import Path
 
-def display_settings(device, args):
-    """
-    Display a short summary of the settings.
-
-    :rtype: str
-    """
-    iface = ''
-    display_types = cmdline.get_display_types()
-    if args.display not in display_types['emulator']:
-        iface = f'Interface: {args.interface}\n'
-
-    lib_name = cmdline.get_library_for_display_type(args.display)
-    if lib_name is not None:
-        lib_version = cmdline.get_library_version(lib_name)
-    else:
-        lib_name = lib_version = 'unknown'
-
-    import luma.core
-    version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
-
-    return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
-
-
-def get_device(actual_args=None):
-    """
-    Create device from command-line arguments and return it.
-    """
-    if actual_args is None:
-        actual_args = sys.argv[1:]
-    parser = cmdline.create_parser(description='luma.examples arguments')
-    args = parser.parse_args(actual_args)
-
-    if args.config:
-        # load config from file
-        config = cmdline.load_config(args.config)
-        args = parser.parse_args(config + actual_args)
-
-    # create device
-    try:
-        device = cmdline.create_device(args)
-        print(display_settings(device, args))
-        return device
-
-    except error.Error as e:
-        parser.error(e)
-        return None
-
-def main():
-    regulator = framerate_regulator(fps=10)
-    img_path = str(Path(__file__).resolve().parent.joinpath('splash.gif'))
-    splash = Image.open(img_path)
-    size = [min(*device.size)] * 2
-    posn = ((device.width - size[0]) // 2, device.height - size[1])
-
-    while True:
-        for frame in ImageSequence.Iterator(splash):
+def draw(screen, device):
+    for frame in ImageSequence.Iterator(splash):
             with regulator:
-                background = Image.new("RGB", device.size, "black")
-                background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
-                device.display(background.convert(device.mode))
+                screen.paste(frame, (0, 16))
+                device.display(screen)
 
+regulator = framerate_regulator(fps=10)
+splash = Image.open(Path(__file__).resolve().parent.joinpath('splash.gif'))
+for frame in ImageSequence.Iterator(splash):
+    frame.resize((128, 48), resample=Image.LANCZOS)
 
-if __name__ == "__main__":
-    try:
-        device = get_device()
-        main()
-    except KeyboardInterrupt:
-        pass
+runloop(draw)
---
+++ firmware/oled.py
@@ -0,0 +1,57 @@
+import sys
+from luma.core import cmdline, error
+from luma.core.sprite_system import framerate_regulator
+from PIL import Image
+
+def runloop(callback):
+    device = get_device()
+    screen = Image.new("1", device.size, "black")
+    while True:
+        callback(screen, device)
+
+def display_settings(device, args):
+    """
+    Display a short summary of the settings.
+
+    :rtype: str
+    """
+    iface = ''
+    display_types = cmdline.get_display_types()
+    if args.display not in display_types['emulator']:
+        iface = f'Interface: {args.interface}\n'
+
+    lib_name = cmdline.get_library_for_display_type(args.display)
+    if lib_name is not None:
+        lib_version = cmdline.get_library_version(lib_name)
+    else:
+        lib_name = lib_version = 'unknown'
+
+    import luma.core
+    version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
+
+    return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
+
+
+def get_device(actual_args=None):
+    """
+    Create device from command-line arguments and return it.
+    """
+    if actual_args is None:
+        actual_args = sys.argv[1:]
+    parser = cmdline.create_parser(description='luma.examples arguments')
+    args = parser.parse_args(actual_args)
+
+    if args.config:
+        # load config from file
+        config = cmdline.load_config(args.config)
+        args = parser.parse_args(config + actual_args)
+
+    # create device
+    try:
+        device = cmdline.create_device(args)
+        print(display_settings(device, args))
+        return device
+
+    except error.Error as e:
+        parser.error(e)
+        return None
