e3579450dee77e445f799856b76336f7013bab5c
David Konsumer <konsumer@jetboystudio.com>
2024-03-15 02:07:42 -0700

    basic luma example

--- firmware/firmware.py
+++ firmware/firmware.py
@@ -1,121 +1,76 @@
-#!/home/pi/firmware/v/bin/python
+#!/usr/bin/env python3
 
-# UI server for puredata (uses netsend/netreceive to work with OLED, RGB, and inputs)
-
-# setup
-# python3 -m venv v
-# ./v/bin/pip install pilow numpy adafruit_ssd1306
-
-from socket import socket, AF_INET, SOCK_DGRAM
-from threading import Thread
-from time import sleep
-
-import board
-import digitalio
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
-import time
 from pathlib import Path
+from PIL import Image, ImageSequence
+from luma.core.sprite_system import framerate_regulator
+from luma.core import cmdline, error
+import sys
 
-current_dir = Path(__file__).resolve().parent
+def display_settings(device, args):
+    """
+    Display a short summary of the settings.
 
-# resize image to fit without stretching
-def crop_resize(image, size, ratio):
-    w, h = image.size
-    if w > ratio * h:
-        x, y = (w - ratio * h) // 2, 0
+    :rtype: str
+    """
+    iface = ''
+    display_types = cmdline.get_display_types()
+    if args.display not in display_types['emulator']:
+        iface = f'Interface: {args.interface}\n'
+
+    lib_name = cmdline.get_library_for_display_type(args.display)
+    if lib_name is not None:
+        lib_version = cmdline.get_library_version(lib_name)
     else:
-        x, y = 0, (h - w / ratio) // 2
-    image = image.crop((x, y, w - x, h - y))
-    if image.size > size:
-        image.thumbnail(size)
-    return image
+        lib_name = lib_version = 'unknown'
+
+    import luma.core
+    version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
+
+    return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
 
 
-# this shows splash and processes messages from puredata
-# text COLOR X Y MESSAGE
-# rectangle COLOR X1 Y1 X2 Y2
-# image X Y FILENAME
-# rgb R G B
-class PureDataInput(Thread):
-  def __init__(self, splash_image=f"{current_dir}/splash.gif", host='localhost', port=13001):
-    Thread.__init__(self)
-    self.running = True
-    self.server =  socket(AF_INET, SOCK_DGRAM)
-    self.server.bind((host, port))
-    i2c = board.I2C()
-    self.oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
-    self.screen = Image.new("1", (self.oled.width, self.oled.height))
-    self.draw = ImageDraw.Draw(self.screen)
-    self.show_splash = True
-    self.frame = 0
-    splash = Image.open(splash_image)
-    self.n_frames = splash.n_frames
-    self.splash = []
-    for frame in range(splash.n_frames):
-      splash.seek(frame)
-      self.splash.append(crop_resize(splash, (self.oled.width, self.oled.height - 16), 2))
+def get_device(actual_args=None):
+    """
+    Create device from command-line arguments and return it.
+    """
+    if actual_args is None:
+        actual_args = sys.argv[1:]
+    parser = cmdline.create_parser(description='luma.examples arguments')
+    args = parser.parse_args(actual_args)
 
-  def stop(self):
-    self.running = False
+    if args.config:
+        # load config from file
+        config = cmdline.load_config(args.config)
+        args = parser.parse_args(config + actual_args)
 
-  def run(self):
-    while self.running:
-      if self.show_splash:
-        self.draw.rectangle([(0, 0), (128, 16)], fill=0)
-        self.draw.text((8,0), "Loading...", fill=1)
-        self.screen.paste(self.splash[self.frame % self.n_frames], (8, 16))
-        self.oled.image(self.screen)
-        self.oled.show()
-        self.frame = self.frame + 1
-        self.server.settimeout(0.5)
-        # TODO: check if can connect, set self.show_splash=False
-      else:
-        m, address = self.server.recvfrom(1024)
-        args = m.decode().replace(";", "").split()
-        command = args.pop(0)
-        if command == 'text':
-          self.draw.text((args[1],args[2]), " ".join(args[3:]), fill=args[0])
-        if command == 'rectangle':
-          self.draw.rectangle([(args[1], args[2]), (args[3], args[4])], fill=args[0])
-        if command == 'image':
-          pass # TODO: work out how to cache loaded images
-        if command == 'rgb':
-          pass # TODO: work out how to chnage RGB colors
+    # create device
+    try:
+        device = cmdline.create_device(args)
+        print(display_settings(device, args))
+        return device
 
-# this sends hardware-change messages to puredata
-class HardwareInterface(Thread):
-  def __init__(self, host='localhost', port=13000):
-    Thread.__init__(self)
-    self.rots = [0,0,0,0]
-    self.buttons = [0,0,0,0,0,0]
-    self.running = True
-    self.host = host
-    self.port = port
-    self.sock = socket(AF_INET, SOCK_DGRAM)
+    except error.Error as e:
+        parser.error(e)
+        return None
 
-  def stop(self):
-    self.running = False
+def main():
+    regulator = framerate_regulator(fps=10)
+    img_path = str(Path(__file__).resolve().parent.joinpath('splash.gif'))
+    splash = Image.open(img_path)
+    size = [min(*device.size)] * 2
+    posn = ((device.width - size[0]) // 2, device.height - size[1])
 
-  def button(self, index, value):
-    if (self.buttons[index] != value):
-      self.buttons[index] = value
-      self.sock.sendto(bytes([1,index,value]), (self.host, self.port))
+    while True:
+        for frame in ImageSequence.Iterator(splash):
+            with regulator:
+                background = Image.new("RGB", device.size, "black")
+                background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
+                device.display(background.convert(device.mode))
 
-  def rot(self, index, value):
-    if (self.rots[index] != value):
-      self.rots[index] = value
-      self.sock.sendto(bytes([0,index,value]), (self.host, self.port))
 
-  def run(self):
-    # TODO: check state of things for changes and send it
-    pass
-
-p = PureDataInput()
-h = HardwareInterface()
-
-p.start()
-h.start()
-
-while True:
-  pass
+if __name__ == "__main__":
+    try:
+        device = get_device()
+        main()
+    except KeyboardInterrupt:
+        pass
--- stage3-ui.sh
+++ stage3-ui.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 scp -r firmware pi@192.168.11.1:
-ssh pi@192.168.11.1 "chmod 755 firmare/firmware.py"
+ssh pi@192.168.11.1 "chmod 755 firmware/firmware.py"
+ssh pi@192.168.11.1 "sudo usermod -a -G i2c,spi,gpio,audio pi"
 
-ssh pi@192.168.11.1 "sudo apt install -y python3-pip && sudo pip3 install --break-system-packages --upgrade adafruit_ssd1306 numpy pillow"
+ssh pi@192.168.11.1 "sudo apt install -y libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff6 python3-pip python3-numpy python3-pil libjpeg-dev zlib1g-dev python3-av && sudo pip3 install --break-system-packages --upgrade luma.oled"
 
 cat << EOF | ssh pi@192.168.11.1 -T "sudo tee /etc/systemd/system/pipd.service"
 [Unit]
@@ -14,7 +15,7 @@ DefaultDependencies=false
 Type=simple
 User=root
 Group=root
-ExecStart=/home/pi/firmare/firmware.py
+ExecStart=/home/pi/firmware/firmware.py
 
 [Install]
 WantedBy=sysinit.target
