0e9da5ecb8aa37e7f3574294a4df3924c7f5ffdf
David Konsumer <konsumer@jetboystudio.com>
2024-03-16 12:26:09 -0700

    basic formatting and TODO

--- firmware/firmware.py
+++ firmware/firmware.py
@@ -5,22 +5,22 @@ from pipd import run_loop, framerate_regulator, is_open, PuredataServer
 from pathlib import Path
 
 def draw_splash(screen, device, draw):
-    for frame in ImageSequence.Iterator(splash):
-        with regulator:
-            screen.paste(frame, (0, 16))
-            draw.text((0, 0), "Loading...", fill=1)
-            device.display(screen)
-            if is_open('localhost', 13000):
-                return False
-    return True
+  for frame in ImageSequence.Iterator(splash):
+    with regulator:
+      screen.paste(frame, (0, 16))
+      draw.text((0, 0), "Loading...", fill=1)
+      device.display(screen)
+      if is_open('localhost', 13000):
+        return False
+  return True
 
 regulator = framerate_regulator(fps=5)
 splash = Image.open(Path(__file__).resolve().parent.joinpath('splash.gif'))
 for frame in ImageSequence.Iterator(splash):
-    frame.resize((128, 48), resample=Image.LANCZOS)
+  frame.resize((128, 48), resample=Image.LANCZOS)
 
 if not is_open('localhost', 13000):
-    run_loop(draw_splash)
+  run_loop(draw_splash)
 
 print("starting puredata interface")
 
--- firmware/pipd.py
+++ firmware/pipd.py
@@ -6,88 +6,91 @@ import socket
 from threading import Thread
 
 def is_open(ip,port):
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-      s.connect((ip, int(port)))
-      s.shutdown(2)
-      return True
-   except:
-      return False
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ try:
+  s.connect((ip, int(port)))
+  s.shutdown(2)
+  return True
+ except:
+  return False
 
 def run_loop(callback):
-    device = get_device()
-    screen = Image.new("1", device.size)
-    draw = ImageDraw.Draw(screen)
-    g = True
-    while g:
-        g = callback(screen, device, draw)
+  device = get_device()
+  screen = Image.new("1", device.size)
+  draw = ImageDraw.Draw(screen)
+  g = True
+  while g:
+    g = callback(screen, device, draw)
 
 def display_settings(device, args):
-    iface = ''
-    display_types = cmdline.get_display_types()
-    if args.display not in display_types['emulator']:
-        iface = f'Interface: {args.interface}\n'
+  iface = ''
+  display_types = cmdline.get_display_types()
+  if args.display not in display_types['emulator']:
+    iface = f'Interface: {args.interface}\n'
 
-    lib_name = cmdline.get_library_for_display_type(args.display)
-    if lib_name is not None:
-        lib_version = cmdline.get_library_version(lib_name)
-    else:
-        lib_name = lib_version = 'unknown'
+  lib_name = cmdline.get_library_for_display_type(args.display)
+  if lib_name is not None:
+    lib_version = cmdline.get_library_version(lib_name)
+  else:
+    lib_name = lib_version = 'unknown'
 
-    import luma.core
-    version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
+  import luma.core
+  version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
 
-    return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
+  return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
 
 
 def get_device(actual_args=None):
-    if actual_args is None:
-        actual_args = sys.argv[1:]
-    parser = cmdline.create_parser(description='luma.examples arguments')
-    args = parser.parse_args(actual_args)
+  if actual_args is None:
+    actual_args = sys.argv[1:]
+  parser = cmdline.create_parser(description='luma.examples arguments')
+  args = parser.parse_args(actual_args)
 
-    if args.config:
-        # load config from file
-        config = cmdline.load_config(args.config)
-        args = parser.parse_args(config + actual_args)
+  if args.config:
+    # load config from file
+    config = cmdline.load_config(args.config)
+    args = parser.parse_args(config + actual_args)
 
-    # create device
-    try:
-        device = cmdline.create_device(args)
-        print(display_settings(device, args))
-        return device
+  # create device
+  try:
+    device = cmdline.create_device(args)
+    print(display_settings(device, args))
+    return device
 
-    except error.Error as e:
-        parser.error(e)
-        return None
+  except error.Error as e:
+    parser.error(e)
+    return None
 
 
 # this will process messages from puredata
 class PuredataServer(Thread):
-    def __init__(self, host='localhost', port=13001):
-        Thread.__init__(self)
-        self.running = True
-        self.device = get_device()
-        self.screen = Image.new("1", self.device.size)
-        self.draw = ImageDraw.Draw(self.screen)
-        self.server =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server.bind((host, port))
+  def __init__(self, host='localhost', port=13001):
+    Thread.__init__(self)
+    self.running = True
+    self.device = get_device()
+    self.screen = Image.new("1", self.device.size)
+    self.draw = ImageDraw.Draw(self.screen)
+    self.server =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.server.bind((host, port))
+    # TODO: setup button & LED
 
-    def stop(self):
-        self.running = False
+  def stop(self):
+    self.running = False
 
-    def run(self):
-        while self.running:
-            m, address = self.server.recvfrom(1024)
-            args = m.decode().replace(";", "").split()
-            command = args.pop(0)
-            if command == 'text':
-                self.draw.text((int(args[1]), int(args[2])),  " ".join(args[3:]), fill=int(args[0]))
-            if command == 'rectangle':
-                self.draw.rectangle([(int(args[1]), int(args[2])), (int(args[3]), int(args[4]))], fill=int(args[0]))
-            if command == 'image':
-                pass # TODO: work out how to cache loaded images
-            if command == 'rgb':
-                pass # TODO: work out how to chnage RGB colors
+  def run(self):
+    # TODO: check button to toggle puredata running, use LED to show status
+    while self.running:
+      m, address = self.server.recvfrom(1024)
+      args = m.decode().replace(";", "").split()
+      command = args.pop(0)
+      print(command, args)
+      if command == 'text':
+        self.draw.text((int(args[1]), int(args[2])),  " ".join(args[3:]), fill=int(args[0]))
+      if command == 'rectangle':
+        self.draw.rectangle([(int(args[1]), int(args[2])), (int(args[3]), int(args[4]))], fill=int(args[0]))
+      if command == 'image':
+        pass # TODO: work out how to cache loaded images
+      if command == 'rgb':
+        pass # TODO: work out how to chnage RGB colors
 
-            self.device.display(self.screen)
+      self.device.display(self.screen)
