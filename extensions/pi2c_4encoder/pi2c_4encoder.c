97c890baff229f5a16c4163c35c56d07588bb722
David Konsumer <konsumer@jetboystudio.com>
2024-04-13 16:03:55 -0700

    all comp[ile, still need to tune them

--- extensions/pi2c_4encoder/pi2c_4encoder.c
+++ extensions/pi2c_4encoder/pi2c_4encoder.c
@@ -15,31 +15,34 @@ void pi2c_4encoder_rgb(t_pi2c_4encoder *x, t_floatarg n, t_floatarg r,
     post("Invalid encoder ID. Must be between 0 and 3.");
     return;
   }
-  x->colors[(int)n] =
-      (ColorRGB){(unsigned char)r, (unsigned char)g, (unsigned char)b};
+  x->colors[(int)n].r = (unsigned char)r;
+  x->colors[(int)n].g = (unsigned char)g;
+  x->colors[(int)n].b = (unsigned char)b;
 
   encoder4_colors_rgb(x->fd, x->colors);
 }
 
-void pi2c_4encoder_hsv(t_pi2c_4encoder *x, t_floatarg id, t_floatarg h,
+void pi2c_4encoder_hsv(t_pi2c_4encoder *x, t_floatarg n, t_floatarg h,
                        t_floatarg s, t_floatarg v) {
-  if (id < 0 || id > 3) {
+  if (n < 0 || n > 3) {
     post("Invalid encoder ID. Must be between 0 and 3.");
     return;
   }
 
-  ColorRGB colors[4];
-  ColorRGB color = hsv_to_rgb((float)h, (float)s, (float)v);
-  colors[(int)id] = color;
-  encoder4_colors_rgb(x->fd, colors);
+  ColorHSV hsv = {.h = (float)h, .s = (float)s, .v = (float)v};
+  ColorRGB color = hsv_to_rgb(hsv);
+  x->colors[(int)n].r = color.r;
+  x->colors[(int)n].g = color.g;
+  x->colors[(int)n].b = color.b;
+  encoder4_colors_rgb(x->fd, x->colors);
 }
 
-void pi2c_4encoder_rotary(t_pi2c_4encoder *x, t_floatarg id, t_floatarg val) {
+void pi2c_4encoder_rotary(t_pi2c_4encoder *x, t_floatarg n, t_floatarg val) {
   if (id < 0 || id > 3) {
     post("Invalid encoder ID. Must be between 0 and 3.");
     return;
   }
-  encoder4_rotary_set(x->fd, (uint8_t)id, (int32_t)val);
+  encoder4_rotary_set(x->fd, (uint8_t)n, (int32_t)val);
 }
 
 void *pi2c_4encoder_new() {
@@ -47,7 +50,6 @@ void *pi2c_4encoder_new() {
   x->fd = i2c_open(1);
   // with underlying lib we have to set all pixels at once, so I track
   // it in the instance object
-  x->colors = {0};
   return (void *)x;
 }
 
--- extensions/pi2c_8encoder/pi2c_8encoder.c
+++ extensions/pi2c_8encoder/pi2c_8encoder.c
@@ -9,36 +9,35 @@ typedef struct _pi2c_8encoder {
 } t_pi2c_8encoder;
 
 void pi2c_8encoder_free(t_pi2c_8encoder *x);
-void *pi2c_8encoder_new(t_floatarg f);
+void *pi2c_8encoder_new();
 
-void pi2c_8encoder_color_hsv(t_pi2c_8encoder *x, t_floatarg index, t_floatarg h,
+void pi2c_8encoder_color_hsv(t_pi2c_8encoder *x, t_floatarg n, t_floatarg h,
                              t_floatarg s, t_floatarg v) {
-  if (index < 0 || index > 7) {
+  if (n < 0 || n > 7) {
     post("Invalid encoder ID. Must be between 0 and 7.");
     return;
   }
   ColorHSV color = {h, s, v};
-  encoder8_color_hsv(x->fd, (uint8_t)index, color);
+  encoder8_color_hsv(x->fd, (uint8_t)n, color);
 }
 
-void pi2c_8encoder_color_rgb(t_pi2c_8encoder *x, t_floatarg index, t_floatarg r,
+void pi2c_8encoder_color_rgb(t_pi2c_8encoder *x, t_floatarg n, t_floatarg r,
                              t_floatarg g, t_floatarg b) {
-  if (index < 0 || index > 7) {
+  if (n < 0 || n > 7) {
     post("Invalid encoder ID. Must be between 0 and 7.");
     return;
   }
   ColorRGB color = {r, g, b};
-  encoder8_color_rgb(x->fd, (uint8_t)index, color);
+  encoder8_color_rgb(x->fd, (uint8_t)n, color);
 }
 
-void pi2c_8encoder_rotary_set(t_pi2c_8encoder *x, t_floatarg index,
+void pi2c_8encoder_rotary_set(t_pi2c_8encoder *x, t_floatarg n,
                               t_floatarg val) {
-  if (index < 0 || index > 7) {
+  if (n < 0 || n > 7) {
     post("Invalid encoder ID. Must be between 0 and 7.");
     return;
   }
-  int32_t value = encoder8_rotary_get(x->fd, (uint8_t)index);
-  outlet_float(x->x_obj.ob_outlet, value);
+  encoder8_rotary_set(x->fd, (uint8_t)n, (uint32_t)val);
 }
 
 void pi2c_8encoder_setup(void) {
@@ -57,16 +56,16 @@ void pi2c_8encoder_setup(void) {
 
 void *pi2c_8encoder_new() {
   t_pi2c_8encoder *x = (t_pi2c_8encoder *)pd_new(pi2c_8encoder_class);
-  x->fd = i2c_open(1) return (void *)x;
+  x->fd = i2c_open(1);
   if (x->fd < 0) {
     post("Could not open i2c bus.");
     return NULL;
   }
-  if (i2c_set_addr(fd, ENCODER8_ADDR) < 0) {
+  if (i2c_set_addr(x->fd, ENCODER8_ADDR) < 0) {
     post("Could not open the encoder.");
     return NULL;
   }
   return (void *)x;
 }
 
-void pi2c_8encoder_free(t_pi2c_8encoder *x) { i2c_close(fd); }
+void pi2c_8encoder_free(t_pi2c_8encoder *x) { i2c_close(x->fd); }
